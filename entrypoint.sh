927eb01bce5cd4eda67370bda2b1069d511a3995
Mestima <mestima@nilored.ru>
2022-09-02 14:17:10 +0300

    repo link & filename updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,9 @@ else
     unzip -o tModLoader.zip
     rm -r tModLoader.zip
 
-    curl -LO https://github.com/PassiveLemon/tmodloader1.4-docker/releases/download/${VERSION}/tmodloader1.4-docker-master.zip
-    unzip -o tmodloader1.4-docker-master.zip
-    rm -r tmodloader1.4-docker-master.zip
+    curl -LO https://github.com/Mestima/tmodloader-docker/releases/download/${VERSION}/tmodloader-docker-master.zip
+    unzip -o tmodloader-docker-master.zip
+    rm -r tmodloader-docker-master.zip
     mkdir contents/ModPacks/
     mkdir contents/Worlds/
     chmod +x startserver.sh
